56ebb3edbbf1a6bcb2bb7ea1beeff77565496108
AuroraD-Hub <aurora99durante@gmail.com>
2023-10-18 17:24:08 +0200

    update markerless

--- tracking_markerless.py
+++ tracking_markerless.py
@@ -6,32 +6,16 @@ import cv2 as cv
 import time
 import socket
 
-def initialize_camera_imu(depth_unit=15/(2**16 - 1), l=640, h=480, fps=60):
-    pipeline = rs.pipeline()
-
-    config = rs.config()
-
-    #config.enable_stream(rs.stream.depth, l, h, rs.format.z16, fps)
-    config.enable_stream(rs.stream.color, l, h, rs.format.bgr8, fps)
-    #config.enable_stream(rs.stream.accel)
-    #config.enable_stream(rs.stream.gyro)
-
-    # Start streaming
-    profile = pipeline.start(config)
-    # Getting the depth sensor's depth scale
-    #sensor_dep = profile.get_device().first_depth_sensor()
-    #sensor_dep.set_option(rs.option.enable_auto_exposure, True) #set auto exposure
-    #sensor_dep.set_option(rs.option.depth_units, depth_unit) #set desired depth_unit
-
-    return pipeline
-
 def main():
     ## INITIALISATION ##
     # Initialise UDP communication
     simulink = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) 
     # Initialise device
-    #depth_unit = 20/(2**16-1) #20 m 
-    pipeline = initialize_camera_imu()
+    pipeline = rs.pipeline()
+    config = rs.config()
+    config.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 30)
+    # Start streaming
+    profile = pipeline.start(config)
 
     ## SETUP ##
     # Create an align object
@@ -43,15 +27,11 @@ def main():
     intrinsics = rgb_profile.get_intrinsics()
     mtx = np.matrix([[intrinsics.fx, 0, intrinsics.ppx], [0, intrinsics.fy, intrinsics.ppy], [0, 0, 1]])
     dist = np.array(intrinsics.coeffs)
-    # Define needed tracking structures
-    old_image = []
-    new_image = []
-    old_corners = []
-    new_corners = []
-    # Define Lucas-Kanade detector parameters
-    lk_params = dict(winSize  = (21, 21), 
-				#maxLevel = 3,
+    # Define Lucas-Kanade Optical Flow parameters
+    lk_params = dict(winSize  = (21, 21),
              	criteria = (cv.TERM_CRITERIA_EPS | cv.TERM_CRITERIA_COUNT, 30, 0.01))
+    # Create colors vector for drawing purpose
+    color = np.random.randint(0, 255, (500, 3))
     # Discard frames at start for assessment
     assessment = 0
 
@@ -62,26 +42,49 @@ def main():
             print("Discarding first frames for camera assessment...")    
             frames = pipeline.wait_for_frames()
             assessment = assessment+1
-        elif assessment=6:
-            print("Initialisation of tracking algorithm: detection of good features.")  
+        elif assessment==5:
+            print("Initialisation of tracking algorithm: detection of good features")  
             aligned_frames = align.process(pipeline.wait_for_frames())
             old_image = np.asanyarray(aligned_frames.get_color_frame().get_data())
             gray_img = cv.cvtColor(old_image, cv.COLOR_RGB2GRAY)
             # Detect features
-            old_corners = cv.goodFeaturesToTrack(gray_img)
+            detector = cv.FastFeatureDetector_create(threshold=25, nonmaxSuppression=True)
+            old_corners = detector.detect(gray_img)
+            old_corners = np.array([x.pt for x in old_corners], dtype=np.float32)
+            mask = np.zeros_like(old_image) # variables for drawing purpose
+            assessment=assessment+1
         else:    
-            print("Tracking camera pose.") 
+            print("Tracking camera pose") 
             aligned_frames = align.process(pipeline.wait_for_frames())
             new_image = np.asanyarray(aligned_frames.get_color_frame().get_data())
             gray_img = cv.cvtColor(new_image, cv.COLOR_RGB2GRAY)
             # Use Optical Flow approach with RANSAC method to get feature motion
-            new_corners, st, err = cv.calcOpticalFlowPyrLK(old_image, new_image, old_corners, None, lk_params)  #shape: [k,2] [k,1] [k,1]
+            new_corners, st, err = cv.calcOpticalFlowPyrLK(old_image, new_image, old_corners, None, **lk_params)
+            st = st.reshape(st.shape[0])
+            old_corners = old_corners[st==1]
+            new_corners = new_corners[st==1]
+            # Draw optical flow
+            #for i, (new, old) in enumerate(zip(new_corners, old_corners)):
+            #    a, b = new.ravel()
+            #    c, d = old.ravel()
+            #    mask = cv.line(mask, (int(a), int(b)), (int(c), int(d)), 0, 2)
+            #    frame = cv.circle(new_image, (int(a), int(b)), 5, color[i].tolist(), -1)
+            #    img = cv.add(frame, mask)
+            #    cv.imshow('Optical flow', img)
             # Retrive camera pose
             E, _ = cv.findEssentialMat(new_corners,old_corners, mtx)
-	        _, R, p, _ = cv.recoverPose(E, new_corners, old_corners, mtx)
+            _, R, p, _ = cv.recoverPose(E, new_corners, old_corners, mtx)
+            #print("Rotation: \n", R)
+            #print("Translation: \n", p)
             # Send via socket camera pose data
-            pose = np.matrix(R,np.transpose(p))
-            simulink.sendto(pose, ("192.168.1.200", 4021))
+            pose = np.concatenate((R,np.transpose(p)))
+            #simulink.sendto(pose, ("192.168.1.200", 4021))
+
+            # If less than 20% of previous features are tracked, perform a new detection 
+            if(old_corners.shape[0] < int(old_corners.shape[0]*0.2)): 
+                print("Detecting new features")    
+                new_corners = detector.detect(gray_img)
+                new_corners = np.array([x.pt for x in new_corners], dtype=np.float32)
 
             old_image = new_image
             old_corners = new_corners
